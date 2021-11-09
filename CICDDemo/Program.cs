2faf41f2e2244eccbfa11ddfbd1eff36f5177b1a
Sammy Wong <66samwon@gafe.molndal.se>
2021-11-09 09:47:36 +0100

    Change text

--- CICDDemo/Program.cs
+++ CICDDemo/Program.cs
@@ -6,7 +6,7 @@ namespace CICDDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello and welcome!");
             Hello();
         }
 
